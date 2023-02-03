d0a952515026934b0ce94d47d43bb5783efd7d33
Bice93 <bianca.cerulo@hotmail.it>
2023-02-03 11:57:36 +0100

    Closure Milestone 1

--- src/org/lessons/java/Evento.java
+++ src/org/lessons/java/Evento.java
@@ -1,6 +1,8 @@
 package org.lessons.java;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Evento {
 	// proprietà:
@@ -11,15 +13,14 @@ public class Evento {
 	
 	// gli attributi devono essere tutti valorizzati nel costruttore , tranne posti prenotati che va inizializzato a 0
 	public Evento(String titolo, LocalDate data, int numeroPostiToatle) throws Exception {
-		
-		
+
 		setTitolo(titolo);
 		setData(data);
 		
 		// Inserire il controllo che il numero di posti totali sia positivo.
-		if (numeroPostiToatle < 0) {
+		if (numeroPostiToatle <= 0) {
 			throw new Exception("Il numero di posti totali deve essere maggiore di 0!");
-		}
+		} 
 		this.numeroPostiTotale = numeroPostiToatle;
 		this.numeroPostiPrenotati = 0;
 	}
@@ -54,5 +55,42 @@ public class Evento {
 		return numeroPostiPrenotati;
 	}
 	
+	/*
+	 * 	Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
+		1. prenota : aggiunge uno ai posti prenotati. Se l’evento è già passato o non ha posti
+		disponibili deve sollevare un’eccezione.	
+	 */
+	public void prenota() throws Exception {
+		if( (data.isBefore(LocalDate.now()) ) || (numeroPostiPrenotati == numeroPostiTotale) ) {
+			throw new Exception("Non è più possibile prenotare per questo evento!");
+		}
+		numeroPostiPrenotati++;
+	}
+	
+	/*
+	 * 	2. disdici : riduce di uno i posti prenotati. Se l’evento è già passato o non ci sono
+		prenotazioni deve sollevare un’eccezione
+	 */
+	public void disdici() throws Exception {
+		LocalDate now = LocalDate.now();
+		if( (data.isBefore(now)) || (numeroPostiPrenotati == 0) ) {
+			throw new Exception("Non è più possibile disdire per questo evento!");
+		}
+		numeroPostiPrenotati--;
+	}
+	
+	/*
+	 * 3. l’override del metodo toString() in modo che venga restituita una stringa
+		contenente: data formattata - titolo
+	 */
+	
+	DateTimeFormatter formattedDate = DateTimeFormatter.ofPattern("dd/MM/yyyy").withLocale(Locale.ITALIAN);
+	
+	@Override
+	public String toString() {
+		return "Evento: [ Data: " + data.format(formattedDate) + " - Titolo: " + titolo + " ]";
+	}
+	
+		
 	
 }
